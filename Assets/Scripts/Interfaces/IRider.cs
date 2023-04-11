74c9b5e8a4ab7459c2a7d409c94ae85afaf04feb
Denis Smirnov <denisabsdenis@gmail.com>
2023-04-11 22:22:04 +0400

    remove IAttachableToBike interface
    rider attaches to bike always

--- Assets/Scripts/Interfaces/IRider.cs
+++ Assets/Scripts/Interfaces/IRider.cs
@@ -1,7 +1,11 @@
+using UnityEngine;
+
 namespace Interfaces
 {
-    public interface IRider : IAttachableToBike, ICameraTraceable
+    public interface IRider : ICameraTraceable
     {
-        
+        // todo: figure out what is anchorPosition
+        void ConnectHands(Rigidbody2D connectedBody, Vector2 anchorPosition);
+        void ConnectFoots(Rigidbody2D connectedBody, Vector2 anchorPosition);
     }
 }
