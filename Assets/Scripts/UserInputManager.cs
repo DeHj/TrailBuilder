d951578f36ac739371089af6dd694050a79d6fd5
Denis Smirnov <denisabsdenis@gmail.com>
2023-04-13 01:42:05 +0400

    cleanup

--- Assets/Scripts/UserInputManager.cs
+++ Assets/Scripts/UserInputManager.cs
@@ -13,46 +13,4 @@ public class UserInputManager : MonoBehaviour
     {
         Time.timeScale = 1 + ratio * (1 / maxSlowDownRatio - 1);
     }
-
-    /*
-    private void HandleForwardRide(float input)
-    {
-        if (input < 0.001f)
-        {
-            Bike.TransmissionJoint.useMotor = false;
-            return;
-        }
-
-        input *= configuration.transmission.speed;
-        if (input > 0
-            && input > -Bike.BackWheel.GetComponentInChildren<Rigidbody2D>().angularVelocity)
-        {
-            var motor = new JointMotor2D
-            {
-                motorSpeed = input,
-                maxMotorTorque = configuration.transmission.force
-            };
-
-            Bike.TransmissionJoint.motor = motor;
-            Bike.TransmissionJoint.useMotor = true;
-        }
-    }
-
-    private void HandleBreaking(float force)
-    {
-        if (force > 0)
-        {
-            BreakWheel(Bike.BackWheel.GetComponentInChildren<Rigidbody2D>(), force, configuration.backBrakeForce);
-            BreakWheel(Bike.FrontWheel.GetComponentInChildren<Rigidbody2D>(), force, configuration.frontBrakeForce);   
-        }
-    }
-
-    /*
-    private static void BreakWheel(Rigidbody2D wheel, float force, float breakForce)
-    {
-        var direction = math.sign(wheel.angularVelocity);
-        var abs = math.abs(wheel.angularVelocity);
-
-        wheel.angularVelocity = direction * math.max(0, abs - force * breakForce);
-    }*/
 }
