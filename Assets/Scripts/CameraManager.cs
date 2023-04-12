5c9620cba564113ba7ee5a24d92fe1ce56bef660
Denis Smirnov <denisabsdenis@gmail.com>
2023-04-13 03:57:45 +0400

    smooth camera manager

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -12,6 +12,8 @@ public class CameraManager : MonoBehaviour
     public float maxSize;
     public float zoomStep;
 
+    public float speed;
+
     private ICameraTraceable ToggledBody { get; set; }
     private Camera _toggledCamera;
 
@@ -30,7 +32,7 @@ public class CameraManager : MonoBehaviour
     {
         if (ToggledBody is not null)
         {
-            CameraPosition = (Vector3)ToggledBody.GetPosition() + new Vector3(offsetX, offsetY, CameraPosition.z);
+            CameraPosition += ((Vector3)ToggledBody.GetPosition() + new Vector3(offsetX, offsetY, CameraPosition.z) - CameraPosition) * speed;
         }
 
         HandleZoom();
